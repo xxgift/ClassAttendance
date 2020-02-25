b8e70caecf77c44a444a821c3f195d3455e9e5da
Thamon Phatrasuteevarodom <thamon1998@gmail.com>
2020-02-25 23:43:01 +0700

    add longclick to delete

--- app/src/main/java/com/mahidol/classattendance/Adapter/MycourseAdapter.kt
+++ app/src/main/java/com/mahidol/classattendance/Adapter/MycourseAdapter.kt
@@ -7,18 +7,28 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.TextView
 import android.widget.Toast
+import androidx.fragment.app.FragmentActivity
+import com.google.firebase.database.FirebaseDatabase
+import com.mahidol.classattendance.Fragments.popup_delete_Fragment
+import com.mahidol.classattendance.Fragments.popup_postdetail_Fragment
 import com.mahidol.classattendance.Models.Course
+import com.mahidol.classattendance.Models.courselistdetail
+import com.mahidol.classattendance.Models.currentuser
 import com.mahidol.classattendance.R
 
 
 class MycourseAdapter (
     val mContext: Context,
+    val mActivity: FragmentActivity,
     val layoutResId: Int,
     val courseList: ArrayList<Course>?
 ) :
     ArrayAdapter<Course>(mContext, layoutResId, courseList!!) {
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var dataReference = FirebaseDatabase.getInstance().getReference("UserProfile").child(
+            currentuser).child("courselist")
 
         val layoutInflator: LayoutInflater = LayoutInflater.from(mContext)
         val view: View = layoutInflator.inflate(layoutResId, null)
@@ -35,6 +45,21 @@ class MycourseAdapter (
         view.setOnClickListener {
             Toast.makeText(mContext, "Selected", Toast.LENGTH_SHORT).show()
         }
+        view.setOnLongClickListener {
+
+            view.animate().setDuration(500).alpha(0f).withEndAction {
+                courselistdetail.removeAt(position)
+                dataReference.setValue(courselistdetail)
+                notifyDataSetChanged()
+                view.alpha = 1.0F
+            }
+            return@setOnLongClickListener true
+        }
         return view
+    }
+
+    private fun showDialog(view: View) {
+        val applypopup = popup_delete_Fragment(view)
+        applypopup.show(mActivity!!.supportFragmentManager, "exampleBottomSheet")
     }
 }
