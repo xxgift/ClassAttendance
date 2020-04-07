959cfbd46385571da91a703576c109c365d8d383
xxgift <g.ttawan@gmail.com>
2020-04-08 01:37:21 +0700

    Auto stash before merge of "xxgift" and "origin/fon"

--- app/src/main/java/com/mahidol/classattendance/LoginActivity.kt
+++ app/src/main/java/com/mahidol/classattendance/LoginActivity.kt
@@ -44,11 +44,11 @@ class LoginActivity : AppCompatActivity() {
 
         val telephonyManager = getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
         val permissionCall = ContextCompat.checkSelfPermission(this,
-                Manifest.permission.READ_PHONE_STATE)
+            Manifest.permission.READ_PHONE_STATE)
         val permissionLocation = ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION)
+            Manifest.permission.ACCESS_FINE_LOCATION)
         val permissionBluetooth = ContextCompat.checkSelfPermission(this,
-                Manifest.permission.BLUETOOTH)
+            Manifest.permission.BLUETOOTH)
 
         if (permissionCall != PackageManager.PERMISSION_GRANTED || permissionLocation != PackageManager.PERMISSION_GRANTED || permissionBluetooth != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_PHONE_STATE, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.BLUETOOTH), 1)
@@ -86,7 +86,7 @@ class LoginActivity : AppCompatActivity() {
                         override fun onPreExecute() {
                             currenttype = null
                             Toast.makeText(this@LoginActivity, "Please wait...", Toast.LENGTH_SHORT)
-                                    .show()
+                                .show()
                         }
 
                         override fun doInBackground(vararg p0: String?): String {
@@ -131,18 +131,18 @@ class LoginActivity : AppCompatActivity() {
                                             applicationContext.startActivity(intent)
                                         } else {
                                             Toast.makeText(
-                                                    this@LoginActivity,
-                                                    "This account is logged in on another device ",
-                                                    Toast.LENGTH_SHORT
+                                                this@LoginActivity,
+                                                "This account is logged in on another device ",
+                                                Toast.LENGTH_SHORT
                                             ).show()
                                         }
                                     }
 
                                 } else {
                                     Toast.makeText(
-                                            this@LoginActivity,
-                                            "Username or Password is not matched",
-                                            Toast.LENGTH_SHORT
+                                        this@LoginActivity,
+                                        "Username or Password is not matched",
+                                        Toast.LENGTH_SHORT
                                     ).show()
                                 }
 
@@ -155,6 +155,7 @@ class LoginActivity : AppCompatActivity() {
 
                     }
                     asyncTask.execute()
+
 
 //                Toast.makeText(this@LoginActivity,">>>>>IMEI:$imei",Toast.LENGTH_SHORT).show()
                 }
